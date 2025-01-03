e35bd9a40c911e3d3890ead035d81dc76b6aebec
Felix <fdilger@outlook.com>
2025-01-03 21:22:29 +0100

    added transformer model, unfinished byte embedding, predictionhead

--- model.py
+++ model.py
@@ -62,10 +62,11 @@ class LayerNorm:
     
         
 class Attention:
-    def __init__(self,n_heads,d_model):
+    def __init__(self,n_heads,d_model,mask):
         self.d_model = d_model
         self.n_heads = n_heads
         self.d_head = d_model//n_heads
+        self.mask = mask
         
     def init(self,key):
         d = self.d_head
@@ -88,7 +89,8 @@ class Attention:
         values = jnp.einsum('btc,ncj->bntj',x,params['value_proj'])
         att = jnp.einsum('bntj,bnsj->bnts',queries,keys)
         att_scaled = att / jnp.sqrt(self.d_model)
-        att_scores = softmax(att_scaled)
+        att_masked = jnp.where(self.mask, -jnp.inf, att_scaled)
+        att_scores = softmax(att_masked)
         att_values = jnp.einsum('bnts,bnsv-> bntv',att_scores,values)
         att_output = jnp.einsum('bntv,nvk->btk', att_values,params['out_proj'])
         return att_output+x
@@ -100,11 +102,12 @@ class Attention:
 class LatentAttention:
     # causal masking
     # TODO: implement decoupled rope
-    def __init__(self,n_heads,d_model,d_latent):
+    def __init__(self,n_heads,d_model,d_latent,mask):
         self.d_model = d_model
         self.n_heads = n_heads
         self.d_head = d_model//n_heads
         self.d_latent = d_latent // n_heads
+        self.mask = mask
         
     def init(self,key):
         keys  = jax.random.split(key, 6)
@@ -136,7 +139,8 @@ class LatentAttention:
         
         att = jnp.einsum('bntj,bnsj->bnts',queries,keys)
         att_scaled = att / jnp.sqrt(self.d_model)
-        att_scores = softmax(att_scaled)
+        att_masked = jnp.where(self.mask, -jnp.inf, att_scaled)
+        att_scores = softmax(att_masked)
         att_values = jnp.einsum('bnts,bnsv-> bntv',att_scores,values)
         att_output = jnp.einsum('bntv,nvk->btk', att_values,params['out_proj'])
         return att_output+x
@@ -163,16 +167,15 @@ class LatentCompression:
 
     
 class TransformerBlock:
-    def __init__(self,d_model,hidden_dim,n_heads,d_latent = 0):
-        att = LatentAttention(n_heads,d_model,d_latent) if d_latent else Attention(n_heads,d_model)
+    def __init__(self,d_model,hidden_dim,n_heads,mask,d_latent = 0):
+        att = LatentAttention(n_heads,d_model,d_latent,mask) if d_latent else Attention(n_heads,d_model,mask)
         self.layers = {'norm1': LayerNorm(d_model),
                        'att'  : att,
                        'norm2': LayerNorm(d_model),
                        'ffn'  : FFN(d_model,hidden_dim,d_model)}
 
-    def init(self):
+    def init(self,key):
         params = {}
-        key = random.PRNGKey(0)
         for name,layer in self.layers.items():
             params[name] = layer.init(key)
         return params
@@ -186,6 +189,19 @@ class TransformerBlock:
         return sum(len(layer) for layer in self.layers.values())
 
 
+class PredictionHead:
+    def __init__(self,d_model,v_size):
+        self.d_model = d_model
+        self.v_size = v_size
+        
+    def init(self,key):
+        keys = jax.random.split(key,2)
+        w = jax.random.normal(keys[0],(self.d_model,self.v_size))
+        b = jax.random.normal(keys[1],(self.v_size,))
+        return {'w':w,'b':b}
+    def __call__(self,params,x):
+        return jnp.einsum('btc,cj->btj',x,params['w']) + params['b']
+         
 class Patching:
     def __init__(self,model,vocab):
         self.model = model
@@ -206,18 +222,45 @@ class Patching:
         return [1 if xi>omega else 0 for xi in ent]
     
 
+class ByteEmbedding:
+    def __init__(self,data,d_model):
+        unique_bytes = sorted(set(data))
+        v_size = len(unique_bytes)
+        self.byte_to_id = {b: jax.nn.one_hot(i,v_size) for i,b in enumerate(unique_bytes)}
+        self.id_to_byte = {i: b for i,b in enumerate(unique_bytes)}
+        self.d_model =d_model
+        
+    def init(self,key):
+        return {'w_embed' : jax.random.normal(key,(v_size,self.d_model))}
+    
+    def encode(self,params,byte):
+        return jnp.einsum('v,vt-> t',self.byte_to_id[byte],params['w_embed'])
+    
+    def decode(self,params,i):
+        return id_to_byte[i]
 
-
-
-
-
-
-
-
-
+class Transformer:
+    def __init__(self,n_layers,d_hidden,d_model,n_heads,v_size,mask,d_latent = 0):
+        self.n_layers = n_layers
+        self.d_hidden = d_hidden
+        self.layers = {}
+        for i in range(n_layers):
+            self.layers['block'+str(i)] = TransformerBlock(d_model,d_hidden,n_heads,mask,d_latent=d_latent)
+        self.layers['pred_head'] = PredictionHead(d_model,v_size)
+        
+    def init(self,key):
+        keys = jax.random.split(key,self.n_layers+1)
+        params = {}
+        for i,(name,layer) in enumerate(self.layers.items()):
+            params[name] = layer.init(keys[i])
+        return params
+    
+    def __call__(self,params,x):
+        for name,layer in self.layers.items():
+            x = layer(params[name],x)
+        return x
 
     
-
 # deepseek-v2
 # d_latent = 4*(d_model // n_heads) 
     
@@ -228,13 +271,15 @@ n_heads = 3
 b_size = 2
 s_len = 3
 d_latent = 3
+n_layers = 2
+v_size = 10
+causal_mask = jnp.triu(jnp.ones((s_len, s_len)), k=1).astype(bool)
 input_shape = (2,3,6)
 key = jax.random.PRNGKey(42)
 data = jax.random.normal(key,input_shape)
-block = TransformerBlock(d_model,hidden_dim,n_heads,d_latent=d_latent)
-print(len(block))
-params = block.init()
-print(block(params,data).shape)
+transformer = Transformer(n_layers,hidden_dim,d_model,n_heads,v_size,causal_mask,d_latent=1)
+params = transformer.init(key)
+print(transformer(params,data).shape)
 
 
         
