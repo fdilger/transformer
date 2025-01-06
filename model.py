3c30bddac9d6dc81f5b294c20766a03d18c5df83
Felix <fdilger@outlook.com>
2025-01-06 21:08:40 +0100

    added rope

--- model.py
+++ model.py
@@ -63,7 +63,30 @@ class LayerNorm:
     def __len__(self):
         return 2*d_model
         
+
+class Rope:
+
+    def __init__(self,d_head,seq_len):
+        self.d_head = d_head
+        self.seq_len = seq_len
+        
+    def init(self):
+        theta = 10000 ** (-2*(jnp.arange(self.d_head//2))/self.d_head) # (self.dhead//2,)
+        positions = jnp.arange(self.seq_len).reshape(-1, 1) # (seq_len,1)
+        cos = jnp.cos(positions * theta)
+        sin = jnp.sin(positions*theta)
+        return cos,sin
     
+    def __call__(self,cos,sin,x):
+        x1, x2 = jnp.split(x, 2, axis=-1)  # (batch_size, seq_len, dim // 2)
+        cos = cos[None, :, :]
+        sin = sin[None, :, :]
+        x_rotated = jnp.concatenate([
+            x1 * cos - x2 * sin,  
+            x1 * sin + x2 * cos   
+        ], axis=-1)
+        return x_rotated
+        
         
 class Attention:
     def __init__(self,n_heads,d_model,mask):
@@ -366,7 +389,20 @@ optimizer = optax.sgd(
 # Initialize it with your params!
 opt_state = optimizer.init(params)
 
-for epoch in range(2000):
+for epoch in range(1):
     params,opt_state,loss = step(params,opt_state,x,targets,transformer)
     print(loss)
 print(softmax(transformer(params,x)))
+
+r = Rope(2,3)
+cos,sin = r.init()
+print(r(cos,sin,jnp.array([[[1,2],[2,3],[4,5]]])))
+## TODO: ##
+# - implement different FFN
+# - add jax.lax.scan optimisations
+# - 
+#
+#
+#
+#
+#
